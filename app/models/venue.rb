9e03911adde361fef9d38f59841079098e9c67d8
Per Christian B. Viken <perchr@northblue.org>
2016-05-10 13:26:38 +0200

    Do the reverse lookup of addresses for Venue
    We assume that all Venues are created with just their coordinates,
    so we do a reverse lookup only.
    
    Check if we need to do a lookup first to avoid hitting rate limits
    in the API used to do lookups.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,3 +1,22 @@
 class Venue < ActiveRecord::Base
+  reverse_geocoded_by :latitude, :longitude do |obj, results|
+    if geo = results.first
+      obj.full_address   = geo.address
+      obj.street_address = geo.street_address
+      obj.city           = geo.city
+      obj.postal_code    = geo.postal_code
+      obj.country        = geo.country
+    end
+  end
 
+  after_validation :reverse_geocode,
+                   if: lambda{|venue| !venue.has_address? && venue.has_coordinates? }
+
+  def has_address?
+    full_address.present?
+  end
+
+  def has_coordinates?
+    latitude.present? && longitude.present?
+  end
 end
