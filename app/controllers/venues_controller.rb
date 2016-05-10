4b83d8db02c568517b3addce87415d54e3ebe097
Per Christian B. Viken <perchr@northblue.org>
2016-05-10 13:43:41 +0200

    Search for Venue by city
    Use some ARel trickery for a database-agnostic case-insensitive search.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,4 +1,11 @@
 class VenuesController < ApplicationController
   def index
+    if params[:q].present?
+      @query  = params[:q].to_s
+      @venues = Venue.search_by_city(@query)
+      render :search
+    else
+      # render default
+    end
   end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -14,6 +14,11 @@ class Venue < ActiveRecord::Base
   after_validation :reverse_geocode,
                    if: lambda{|venue| !venue.has_address? && venue.has_coordinates? }
 
+  def self.search_by_city(query)
+    # https://robb.weblaws.org/2013/12/05/yes-rails-supports-case-insensitive-database-queries/
+    where(arel_table[:city].matches("#{query}%"))
+  end
+
   def has_address?
     full_address.present?
   end
