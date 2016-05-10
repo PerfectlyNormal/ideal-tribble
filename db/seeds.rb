177a65cbcae398c9ccda38e9ca17b59e2078ea7d
Per Christian B. Viken <perchr@northblue.org>
2016-05-10 13:27:53 +0200

    Load seed data from JSON

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# This should be handled a bit better, as every record creation
+# triggers a reverse lookup of the latitude and longitude, and
+# can easily hit all sorts of rate limiting in various available APIs.
+
+venues = JSON.parse(File.read(Rails.root.join("db/venues.json")))
+venues.each do |source|
+  Venue.create({
+    name:      source["name"],
+    latitude:  source["lat"],
+    longitude: source["lng"]
+  })
+end
