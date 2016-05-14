b3ad581a354781e4ee43b81c30bc3df58e925222
Per Christian B. Viken <perchr@northblue.org>
2016-05-14 23:29:20 +0200

    Remove outdated comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This should be handled a bit better, as every record creation
-# triggers a reverse lookup of the latitude and longitude, and
-# can easily hit all sorts of rate limiting in various available APIs.
-
 venues = JSON.parse(File.read(Rails.root.join("db/venues.json")))
 venues.each do |source|
   Venue.create({
